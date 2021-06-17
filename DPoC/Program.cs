368a66ad33a1df9e01e4ccdc4cdeadeb7103b570
chempik <ruslansemenyak911@gmail.com>
2021-06-17 15:12:47 +0300

    first commit

---
+++ DPoC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DPoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
